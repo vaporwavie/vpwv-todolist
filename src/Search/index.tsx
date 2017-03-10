5d129c7e84b57a55e3ac1b6105df92e3402ec650
Luiz Nickel <luizaugustonickel@gmail.com>
2017-03-10 16:12:29 -0300

    Adds search source folder

---
+++ src/Search/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+const s = require("./Search.scss");
+
+interface Props {
+    onChange: (inputText: string) => any;
+}
+
+export default class Search extends React.Component<Props, {}> {
+    onChange(e: any) {
+        this.props.onChange(e.target.value);
+    }
+    render() {
+        let props = Object.assign({}, this.props);
+
+        return (
+            <div>
+                <label htmlFor="searchTask">Search a task</label>
+                <input onChange={this.onChange.bind(this)}/>
+            </div>
+        );
+    }
+}
